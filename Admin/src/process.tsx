014664f0f144bb1d297dbc6d0a83943dce33379a
KamishiCode <blackalphalotus@gmail.com>
2024-08-02 11:41:50 +0800

    initialize admin webpack

--- Admin/src/process.tsx
+++
@@ -1,5 +0,0 @@
-export default function Process(){
-    return (<>
-            
-    </>)
-}
---
+++ Webpack_admin/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports={
+    mode: "development", 
+    entry: "./index.tsx", 
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+        publicPath: '/',
+    },
+    
+    target: "web",
+    devServer: {
+        port: "5176",
+        open: false,
+        hot: true ,
+        liveReload: true,
+        historyApiFallback: true
+    },
+    resolve: {
+        extensions: ['.js','.ts','.tsx', '.json']
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,    //kind of file extension this rule should look for and apply in test
+                exclude: /node_modules/, //folder to be excluded
+                use:  'babel-loader' //loader which we are going to use
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    }
+}
