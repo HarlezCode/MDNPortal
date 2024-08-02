f871a5cff88147a2433712c0505d471b727d8aaa
KamishiCode <blackalphalotus@gmail.com>
2024-08-02 11:27:10 +0800

    Merge index to components css

--- Webpack_client/index.tsx
+++ Webpack_client/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './src/App'
-import "./src/index.css"
+import "./src/components"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
